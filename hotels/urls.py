f95bf302fd880c12dadcfb9f78fe5fe9cc53f331
Amir Shabani <129328075+amirshabaniv@users.noreply.github.com>
2024-03-17 02:26:23 +0330

    Update urls.py

--- hotels/urls.py
+++ hotels/urls.py
@@ -4,14 +4,13 @@ from django.urls import path, include
 
 
 router = routers.DefaultRouter()
+
 router.register('hotels/(?P<city_name>[^/.]+)', views.CityHotelsViewSet, basename='city_hotels')
+
 router.register('home', views.HomeViewSet, basename='home')
-
-hotels_router = routers.NestedDefaultRouter(router, 'hotels/(?P<city_name>[^/.]+)')
-hotels_router.register("rooms", views.RoomViewSet, basename="rooms")
-
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(hotels_router.urls)),
+    path('hotels/<slug:city_name>/<int:hotel_id>/rooms/', views.RoomsAPIView.as_view()),
+    path('hotels/<slug:city_name>/<int:hotel_id>/rooms/<int:room_id>/', views.RoomAPIView.as_view()),
 ]
